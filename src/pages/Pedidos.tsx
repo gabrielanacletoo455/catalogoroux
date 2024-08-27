1ce1c03d14085ce97560787edee80fc768ac8f9c
Gabriel anacleto <gabrielanacleto159@live.com>
2024-08-27 03:11:23 -0300

    att

--- src/pages/Pedidos.tsx
+++ src/pages/Pedidos.tsx
@@ -1,6 +1,6 @@
+import { useEffect, useState } from 'react';
 import { Loader2 } from 'lucide-react';
 import Logo from '@/assets/banner.jpeg';
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { PedidosType } from '@/@types/Pedidos';
 import { GetPedidos, CriarPedido, AtualizarPedido, ExcluirPedido } from '@/services/Pedidos';
@@ -12,11 +12,13 @@ const Pedidos = () => {
     const [loading, setLoading] = useState(false);
     const [pedidos, setPedidos] = useState<PedidosType[]>([]);
     const [formData, setFormData] = useState<PedidosType>({
-        cliente: { nome: '' }, // Inicialmente vazio
-        produtos: [], // Lista de produtos
+        cliente: { nome: '' },
+        produtos: [],
     });
     const [editingId, setEditingId] = useState<string | null>(null);
     const [show, setShow] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedPedido, setSelectedPedido] = useState<PedidosType | null>(null);
 
     useEffect(() => {
         fetchPedidos();
@@ -27,7 +29,7 @@ const Pedidos = () => {
         try {
             const data = await GetPedidos();
             if (data && data.items) {
-                setPedidos(data.items as unknown as PedidosType[]); // Certifique-se de que data.items é um PedidosType[]
+                setPedidos(data.items as unknown as PedidosType[]);
             }
         } catch (error) {
             console.error('Erro ao buscar pedidos:', error);
@@ -50,6 +52,7 @@ const Pedidos = () => {
             if (result && result.status === 200) {
                 setPedidos(prev => prev.map(p => p.id === editingId ? { ...formData, id: editingId } : p));
                 resetForm();
+                setModalVisible(false);
             }
         }
     };
@@ -62,8 +65,10 @@ const Pedidos = () => {
     };
 
     const handleEditClick = (pedido: PedidosType) => {
+        setSelectedPedido(pedido);
         setEditingId(pedido.id || '');
         setFormData(pedido);
+        setModalVisible(true);
     };
 
     const resetForm = () => {
@@ -90,59 +95,100 @@ const Pedidos = () => {
 
             <div>
                 <div className="mt-2 p-2 mx-auto">
-                
                     <div className='flex items-center justify-between'>
-                    <h1 className="text-3xl tracking-tighter font-bold">Pedidos</h1>
+                        <h1 className="text-3xl tracking-tighter font-bold">Pedidos</h1>     
+                        <Button onClick={() => setShow(!show)}>Cadastrar pedido</Button>
 
-                <div>
-                    <Button onClick={() => setShow(!show)}>Cadastrar pedido</Button>
-
-                    {show && (
-                        <div>
-<Input
-                        type="text"
-                        placeholder="Cliente"
-                        value={formData.cliente.nome || ''}
-                        onChange={(e) => setFormData({ ...formData, cliente: { nome: e.target.value } })}
-                        className="border p-2 w-full mb-2"
-                    />
-                    <textarea
-                        placeholder="Descrição do produto"
-                        value={formData.produtos.map(p => p.nome).join(', ')} // Ajuste para acessar nome diretamente
-                        onChange={(e) => setFormData({
-                            ...formData,
-                            produtos: [{ nome: e.target.value, quantidade: 1 }] // Ajuste para corresponder ao tipo correto
-                        })}
-                        className="border p-2 w-full mb-2"
-                    />
-                    
-                    <button
-                        onClick={editingId ? handleUpdate : handleCreate}
-                        className="bg-green-700 text-white p-2" >
-                        {editingId ? 'Atualizar Pedido' : 'Criar Pedido'}
-                    </button>
-                        </div>
-                    )}
-             
-                </div>
+                        {show && (
+                            <div>
+                                <Input
+                                    type="text"
+                                    placeholder="Cliente"
+                                    value={formData.cliente.nome || ''}
+                                    onChange={(e) => setFormData({ ...formData, cliente: { nome: e.target.value } })}
+                                    className="border p-2 w-full mb-2"
+                                />
+                                <textarea
+                                    placeholder="Descrição do produto"
+                                    value={formData.produtos.map(p => p.nome).join(', ')}
+                                    onChange={(e) => setFormData({
+                                        ...formData,
+                                        produtos: [{ nome: e.target.value, quantidade: 1 }]
+                                    })}
+                                    className="border p-2 w-full mb-2"
+                                />
+                                
+                                <Button
+                                    onClick={editingId ? handleUpdate : handleCreate}
+                                    className="bg-green-700 text-white p-2"
+                                >
+                                    {editingId ? 'Atualizar Pedido' : 'Criar Pedido'}
+                                </Button>
+                            </div>
+                        )}
                     </div>
-                   
                 </div>
 
-                <Card className='w-[95%]'>
+                <Card className='w-[95%] mx-auto'>
                     {pedidos.map(pedido => (
-                        <div key={pedido.id} className="border p-2 mb-2">
-                            <div>Cliente: {pedido.cliente.nome}</div>
-                            <div>Produtos: {pedido.produtos.map(p => p.nome).join(', ')}</div>
-                            <button onClick={() => handleEditClick(pedido)} className="bg-blue-700 text-white p-1 mr-2">Editar</button>
-                            <button onClick={() => handleDelete(pedido.id || '')} className="bg-red-700 text-white p-1">Excluir</button>
+                        <div key={pedido.id} className="p-2 mb-2">
+                            <div className='flex items-center justify-between'>
+                                {pedido.cliente.nome}
+                                <Button 
+                                    variant="link" 
+                                    className="underline text-blue-600"
+                                    onClick={() => handleEditClick(pedido)}
+                                >
+                                    Saber mais
+                                </Button>
+                            </div>
                         </div>
                     ))}
                 </Card>
             </div>
 
-            <button className="bg-red-700 text-white p-4 w-full absolute bottom-0 flex items-center justify-center space-x-2"
-                onClick={() => window.history.back()}>
+            {/* Modal */}
+            {modalVisible && (
+                <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
+                    <div className="bg-white p-2 rounded-lg w-[95%]">
+                        <h2 className="text-2xl font-bold mb-4">Editar Pedido</h2>
+                        <Input
+                            type="text"
+                            placeholder="Cliente"
+                            value={formData.cliente.nome || ''}
+                            onChange={(e) => setFormData({ ...formData, cliente: { nome: e.target.value } })}
+                            className="border p-2 w-full mb-2"
+                        />
+                        <textarea
+                            placeholder="Descrição do produto"
+                            value={formData.produtos.map(p => p.nome).join(', ')}
+                            onChange={(e) => setFormData({
+                                ...formData,
+                                produtos: [{ nome: e.target.value, quantidade: 1 }]
+                            })}
+                            className="border p-2 w-full mb-2"
+                        />
+                        <div className="flex justify-end space-x-2">
+                            <Button 
+                                onClick={() => {
+                                    handleUpdate();
+                                    setModalVisible(false);
+                                }}>
+                                Atualizar Pedido
+                            </Button>
+                            <Button variant="destructive"
+                                onClick={() => setModalVisible(false)}>
+                                Fechar
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            <button 
+                className="bg-red-700 text-white p-4 w-full absolute bottom-0 flex items-center justify-center space-x-2"
+                onClick={() => window.history.back()}
+            >
                 Voltar
             </button>
         </div>
